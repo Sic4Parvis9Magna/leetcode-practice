687308f97754c847d0152b436d4b673bc7521b16
Sic4Parvis9Magna <maksvyacheslavovich@gmail.com>
2023-08-14 01:59:15 +0900

    add 'binary_search/GuessNumberHigherOrLower.java' , status completed

---
+++ binary_search/GuessNumberHigherOrLower.java
@@ -0,0 +1,71 @@
+/**
+ * We are playing the Guess Game. The game is as follows:
+ * 
+ * I pick a number from 1 to n. You have to guess which number I picked.
+ * 
+ * Every time you guess wrong, I will tell you whether the number I picked is
+ * higher or lower than your guess.
+ * 
+ * You call a pre-defined API int guess(int num), which returns three possible
+ * results:
+ * 
+ * -1: Your guess is higher than the number I picked (i.e. num > pick).
+ * 1: Your guess is lower than the number I picked (i.e. num < pick).
+ * 0: your guess is equal to the number I picked (i.e. num == pick).
+ * Return the number that I picked.
+ * 
+ * Example 1:
+ * Input: n = 10, pick = 6
+ * Output: 6
+ * 
+ * Example 2:
+ * Input: n = 1, pick = 1
+ * Output: 1
+ * 
+ * Example 3:
+ * Input: n = 2, pick = 1
+ * Output: 1
+ * 
+ * Constraints:
+ * 
+ * 1 <= n <= 2^31 - 1
+ * 1 <= pick <= n
+ * 
+ * GuessNumberHigherOrLower
+ * STATUS: COMPETED
+ * TODOS: ADD TESTS
+ */
+public class GuessNumberHigherOrLower {
+
+}
+
+/**
+ * Forward declaration of guess API.
+ * 
+ * @param num your guess
+ * @return -1 if num is higher than the picked number
+ *         1 if num is lower than the picked number
+ *         otherwise return 0
+ *         int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            int pick = guess(middle);
+            if (pick == 0) {
+                return middle;
+            } else if (pick == -1) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+}
