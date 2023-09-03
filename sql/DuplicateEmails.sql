8d9e5053f6dedefb007585a6cd38d1077bcf634c
Sic4Parvis9Magna <maksvyacheslavovich@gmail.com>
2023-09-03 18:30:42 +0900

    add 'sql/DuplicateEmails.sql'  status completed

---
+++ sql/DuplicateEmails.sql
@@ -0,0 +1,66 @@
+/*
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+*/
+
+Create table If Not Exists Person (id int, email varchar(255));
+Truncate table Person;
+insert into Person (id, email) values ('1', 'a@b.com');
+insert into Person (id, email) values ('2', 'c@d.com');
+insert into Person (id, email) values ('3', 'a@b.com');
+
+
+/*
+    STATUS: COMPLETED
+*/
+
+-- slow solution
+SELECT DISTINCT p1.email as Email FROM Person p1, Person p2 
+WHERE p1.id <> p2.id AND
+      p1.email = p2.email;
+
+-- another aproach also slow
+SELECT p.email AS email FROM Person p
+GROUP BY p.email 
+HAVING COUNT(*) > 1;
+
+-- another aproach also slow
+SELECT DISTINCT p1.email AS email FROM Person p1
+INNER JOIN Person p2
+WHERE p1.id <> p2.id AND p1.email = p2.email;
