eca45ecb07e598b3d90e286b11fdb4a35e761e15
Sic4Parvis9Magna <maksvyacheslavovich@gmail.com>
2023-10-08 20:48:34 +0900

    add 'data-structure-tree/BrinaryTreeFromInorderAndPostOrder.java' status completed

--- data-structure-tree/BrinaryTreeFromInorderAndPostOrder.java
+++ data-structure-tree/BrinaryTreeFromInorderAndPostOrder.java
@@ -1,8 +1,12 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import BrinaryTreeFromInorderAndPostOrder.TreeNode;
@@ -28,18 +32,21 @@ import BrinaryTreeFromInorderAndPostOrder.TreeNode;
  * Each value of postorder also appears in inorder.
  * inorder is guaranteed to be the inorder traversal of the tree.
  * postorder is guaranteed to be the postorder traversal of the tree.
+ * 
+ * STATUS: COMPLETED
+ * 
  */
 
-// TODO finish the algorithm
-class BrinaryTreeFromInorderAndPostOrderTest {
+class BrinaryTreeFromInorderAndPostOrder {
     public static void main(String[] args) {
+        // TODO finish tests
         test1();
         test2();
     }
 
     public static void test1() {
         // given
-        BrinaryTreeFromInorderAndPostOrder sut = new BrinaryTreeFromInorderAndPostOrder();
+        Solution sut = new Solution();
         int[] inorder = new int[] { 9, 3, 15, 20, 7 };
         int[] postorder = new int[] { 9, 15, 7, 20, 3 };
         List<Integer> expectedResult = Arrays.asList(3, 9, 20, null, null, 15, 7);
@@ -68,41 +75,38 @@ class BrinaryTreeFromInorderAndPostOrderTest {
     }
 }
 
-class BrinaryTreeFromInorderAndPostOrder {
+class Solution {
+    private static int postorderIndex = 0;
+
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        Map<Integer, TreeNode> nodeMap = new HashMap<>();
-        for (int i : postorder) {
-            nodeMap.put(i, new TreeNode(i));
-        }
-
-        int length = postorder.length;
-
-        int inordercounterIndex = 0;
-        int postordercounterIndex = 0;
-        for (int i = 0; i < length; i++) {
-            int nextInorder = inorder[inordercounterIndex];
-            int nextPostorder = postorder[postordercounterIndex];
-            if (nextInorder == nextPostorder) {
-                System.out.println("left node with value = ", nextInorder);
-                inordercounterIndex++;
-                postordercounterIndex++;
-                continue;
-            }
-            if (i != 0) {
-                if (nextInorder != nextPostorder) {
-                    TreeNode nextRoot = nodeMap.get(inordercounterIndex);
-                    TreeNode nextLeft = nodeMap.get(inordercounterIndex - 1);
-                    nextRoot.left = nextLeft;
-                } else {
-                    TreeNode nextRoot = nodeMap.get(inordercounterIndex);
-                    TreeNode nextLeft = nodeMap.get(inordercounterIndex - 1);
-                }
-            }
-        }
-
-        return nodeMap.get(postorder[length - 1]);
+        Map<Integer, Integer> inOrderMap = getValueIndexMap(inorder);
+        postorderIndex = postorder.length - 1;
+        return generateTree(0, postorderIndex, inOrderMap, postorder);
     }
 
+    private TreeNode generateTree(int left, int right, Map<Integer, Integer> inOrderMap, int[] postorder) {
+        if (left > right) {
+            return null;
+        }
+
+        int nextVal = postorder[postorderIndex];
+        postorderIndex -= 1;
+        TreeNode root = new TreeNode(nextVal);
+        int nextInOrderIndex = inOrderMap.get(nextVal);
+
+        root.right = generateTree(nextInOrderIndex + 1, right, inOrderMap, postorder);
+        root.left = generateTree(left, nextInOrderIndex - 1, inOrderMap, postorder);
+
+        return root;
+    }
+
+    private Map<Integer, Integer> getValueIndexMap(int[] arr) {
+        Map<Integer, Integer> resuult = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            resuult.put(arr[i], i);
+        }
+        return resuult;
+    }
 }
 
 class TreeNode {
